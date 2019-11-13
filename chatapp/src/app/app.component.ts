482edf10c6601e305c2a8fc3c247ea9b42a0cb6b
Szabó Péter <szabopeter@pro.local>
2019-11-13 15:24:50 +0100

    [chatapp poc] init chat app template

--- chatapp/src/app/app.component.ts
+++ chatapp/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Observable } from "rxjs";
+import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { takeLast, tap, map, flatMap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-  title = 'chatapp';
+  title = "chatapp";
+  itemsRef: AngularFireList<any>;
+  items: Observable<Message[]>;
+  messages: Message[] = [];
+  name: any;
+  msgVal: string = "";
+
+  constructor(public db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.itemsRef = db.list("/",ref=>ref.limitToLast(4));
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(c => ({ user: c.payload.val().name, message: c.payload.val().message }));
+      })
+    );
+    this.afAuth.user.subscribe(x => {
+      this.name = x;
+      this.items.subscribe((array)=>{
+        array.forEach(element => {
+          this.messages.push(element);
+        });
+        console.log(`this is an array ${array}`)
+      }
+      );
+    });
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  chatSend(message: string) {
+    this.items[0].push({ message: message, name: this.name.facebook.displayName });
+    this.msgVal = "";
+  }
+}
+
+interface Message {
+  user: string;
+  message: string;
+}
+
+interface MyResponse {
+  message: Message[];
 }
--- chatapp/src/app/app.module.ts
+++ chatapp/src/app/app.module.ts
@@ -1,8 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJM4FZkLkXgiSpKF2h0wRAxY-qD7ieXK0",
+  authDomain: "chat-eszapet.firebaseapp.com",
+  databaseURL: "https://chat-eszapet.firebaseio.com",
+  projectId: "chat-eszapet",
+  storageBucket: "chat-eszapet.appspot.com",
+  messagingSenderId: "829717351016",
+  appId: "1:829717351016:web:9b2162fccc09bf190449dd"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +27,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    // AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
