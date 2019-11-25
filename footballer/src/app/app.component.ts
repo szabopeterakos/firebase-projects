9e1abe434ab4cf4eab7dafc1b6e5d8d1f94a6d40
Szabó Péter <szabopeter@pro.local>
2019-11-25 15:13:43 +0100

    add database models

--- footballer/src/app/app.component.ts
+++ footballer/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { auth } from "firebase/app";
 import { map, tap } from "rxjs/operators";
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: "app-root",
@@ -13,25 +13,37 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class AppComponent {
   title = "footballer";
-  messages: Observable<any>;
-  users: Observable<any>;
-  appSettings: Observable<any>;
+  messages: Observable<Message[]>;
+  users: Observable<User[]>;
+  appSettings: Observable<AppSetting[]>;
   items: Observable<Message[]>;
   name: any;
   msgVal: string = "";
 
   constructor(public db: AngularFirestore, public afAuth: AngularFireAuth) {
-    this.messages = db.collection("messages").valueChanges();
-    this.users = db.collection("users").valueChanges();
-    this.appSettings = db.collection("app-settings").valueChanges();
+    this.messages = db.collection<Message>("messages").valueChanges();
+    this.users = db.collection<User>("users").valueChanges();
+    this.appSettings = db.collection<AppSetting>("app-settings").valueChanges();
   }
 }
 
 interface Message {
-  user: string;
+  id: string;
   message: string;
 }
 
-interface MyResponse {
-  message: Message[];
+interface User {
+  displayLanguage: string;
+  displayName: string;
+  guests: number;
+  id: number;
+  isAdmin: boolean;
+  status: string;
+}
+
+interface AppSetting {
+  date: Date;
+  eventStatus: string;
+  eventTitle: string;
+  minTreshold: number;
 }
