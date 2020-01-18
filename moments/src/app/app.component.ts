7a42aea39bfbb58298f73261eac2339c2cb248f8
szabopeter <szabopeterakos@gmail.com>
2020-01-18 11:45:50 +0100

    add data

--- moments/src/app/app.component.ts
+++ moments/src/app/app.component.ts
@@ -21,8 +21,30 @@ export class AppComponent implements OnInit {
     this.times[2] = moment_tz(this.startTimestamps[2])
       .tz("UTC")
       .format(this.DATE_FORMAT);
+
+    this.timesAmerica[0] = moment_tz(this.startTimestamps[0])
+      .tz("America/New_York")
+      .format(this.DATE_FORMAT);
+    this.timesAmerica[1] = moment_tz(this.startTimestamps[1])
+      .tz("America/New_York")
+      .format(this.DATE_FORMAT);
+    this.timesAmerica[2] = moment_tz(this.startTimestamps[2])
+      .tz("America/New_York")
+      .format(this.DATE_FORMAT);
+
+    this.timesVladi[0] = moment_tz(this.startTimestamps[0])
+      .tz("Asia/Vladivostok")
+      .format(this.DATE_FORMAT);
+    this.timesVladi[1] = moment_tz(this.startTimestamps[1])
+      .tz("Asia/Vladivostok")
+      .format(this.DATE_FORMAT);
+    this.timesVladi[2] = moment_tz(this.startTimestamps[2])
+      .tz("Asia/Vladivostok")
+      .format(this.DATE_FORMAT);
   }
 
   title = "moments";
   times = [];
+  timesAmerica = [];
+  timesVladi = [];
 }
