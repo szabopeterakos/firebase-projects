c74e83c3de04e3589391438292406ef609ea00ac
szabopeter <szabopeterakos@gmail.com>
2020-01-18 11:27:17 +0100

    moment tester

--- moments/src/app/app.component.ts
+++ moments/src/app/app.component.ts
@@ -8,12 +8,17 @@ import * as moment_tz from "moment-timezone";
 })
 export class AppComponent implements OnInit {
   DATE_FORMAT = "YYYY.MM.DD HH:mm Z";
+  // 2018-02-13
   ngOnInit(): void {
-    this.times[0] = moment().format(this.DATE_FORMAT);
-    this.times[1] = moment_tz()
-      .tz("America/New_York")
+    this.times[0] = moment_tz(1518480060000)
+      .tz("UTC")
       .format(this.DATE_FORMAT);
-    this.times[2] = moment().format(this.DATE_FORMAT);
+    this.times[1] = moment_tz(1518519600000)
+      .tz("UTC")
+      .format(this.DATE_FORMAT);
+    this.times[2] = moment_tz(1518561000000)
+      .tz("UTC")
+      .format(this.DATE_FORMAT);
   }
 
   title = "moments";
