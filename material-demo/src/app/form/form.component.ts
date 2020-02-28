7e29037eac5fe6555b767df016e07d9f05ac4867
szabopeter <szabopeterakos@gmail.com>
2020-02-28 16:27:16 +0100

    material 0.3 autocomplete forms

--- material-demo/src/app/form/form.component.ts
+++ material-demo/src/app/form/form.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.sass']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.sass"]
 })
 export class FormComponent implements OnInit {
+  selectedValue;
+  list: { name: string }[] = [
+    { name: "one" },
+    { name: "two" },
+    { name: "who" }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  displFn(subj) {
+    return subj.name.toUpperCase();
   }
-
 }
