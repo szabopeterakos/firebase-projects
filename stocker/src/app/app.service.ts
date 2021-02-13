75584ca64c57fb7e8184276110fb04d025a77cf0
Péter Szabó <peter.szabo@infinitect.hu>
2021-02-13 22:15:58 +0100

    stock api

--- stocker/src/app/app.service.ts
+++ stocker/src/app/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject, Observable } from "rxjs";
 import { map, catchError, tap, flatMap } from "rxjs/operators";
 import { of } from "rxjs";
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -11,28 +12,27 @@ export class AppService {
   private message: BehaviorSubject<string> = new BehaviorSubject<string>(null);
   public message$: Observable<string> = this.message.asObservable();
   // public data$: Observable<any>;
+  headers: HttpHeaders = new HttpHeaders({
+    "X-RapidAPI-Key": environment.apiKey,
+  });
 
-  constructor(private http: HttpClient) {
-    // this.getData();
-  }
+  constructor(private http: HttpClient) {}
 
   // getData() {
   // this.http.get("https://jsonplaceholder.typicode.com/users").subscribe(
   //   (res) => console.log(res),
   //   (err) => this.errorHandler(err)
   // );
+
   public data$ = this.http
-    .get("https://jsonplaceholder.typicode.com/users")
+    .get(
+      "https://morning-star.p.rapidapi.com/stock/v2/get-realtime-data?performanceId=0P0000OQN8",
+      { headers: this.headers }
+    )
     .pipe(
-      // tap((data) => {
-      //   console.log(
-      //     "🚀 ~ file: app.service.ts ~ line 27 ~ AppService ~ tap ~ data",
-      //     data
-      //   );
+      // map((users: any[]) => {
+      //   return users.map((user) => user.name);
       // }),
-      map((users: any[]) => {
-        return users.map((user) => user.name);
-      }),
       tap((data) => {
         console.log(
           "🚀 ~ file: app.service.ts ~ line 27 ~ AppService ~ tap ~ data",
@@ -43,7 +43,6 @@ export class AppService {
         return of(this.errorHandler(error));
       })
     );
-  // }
 
   setMessage(newMessage) {
     this.message.next(newMessage);
